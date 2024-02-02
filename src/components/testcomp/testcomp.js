bdb4fda9b27b3b6d08e614660f35b72b3190fdd9
ThamaraiselvamC <thamaraiselvamc@gmail.com>
2024-02-02 13:57:38 +0530

    H1 Repalce

--- src/components/testcomp/testcomp.js
+++ src/components/testcomp/testcomp.js
@@ -1,7 +1,7 @@
-export default function TestComp(){
-    return(
-        <div>
-            <h1>Hello from signup</h1>
-        </div>
-    )
+export default function TestComp() {
+  return (
+    <div>
+      <h1>Hello Test</h1>
+    </div>
+  );
 }
